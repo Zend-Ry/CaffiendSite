ffe7db5ff22e839ef97415586b2eb1623a0f6548
Riley <riley.daigle2000@gmail.com>
2025-03-22 23:01:48 -0400

    set logos to wrong path

--- docs/assets/main-D1bjE4_j.js
+++ docs/assets/main-D1bjE4_j.js
@@ -8311,7 +8311,7 @@ document.querySelector("#navigation").innerHTML = `
 <div id="navigation-desktop" class="hf-sizing">
     <div class="content">
             <div>
-                <img id="logo" src="assets/Logo-Caffiend-Desktop.png" alt="Caffiend Games logo"/>
+                <img id="logo" src="Logo-Caffiend-Desktop.png" alt="Caffiend Games logo"/>
             </div>
             <div id="nav">
                 <a class="nav-link" href="#about-us">About Us</a>
@@ -8326,7 +8326,7 @@ document.querySelector("#navigation").innerHTML = `
 <div id="navigation-mobile" class="hf-sizing">
     <div class="content">
         <div>
-            <img id="logo" src="assets/Logo-Caffiend-Mobile.png" alt="Caffiend Games logo"/>
+            <img id="logo" src="Logo-Caffiend-Mobile.png" alt="Caffiend Games logo"/>
         </div>
         <div id="hamburger-menu">
             <i class="fa-solid fa-bars fa-2xl"></i>
